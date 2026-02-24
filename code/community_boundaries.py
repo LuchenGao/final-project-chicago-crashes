17f7180b25736c4da6f77d3ed38fa122bb76d316
Luchen Gao <gaoluchen@gaoluchendeMacBook-Air.local>
2026-02-23 20:47:31 -0600

    Add community_boundaries.py

---
+++ code/community_boundaries.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+path = "../data/raw-data/Boundaries_-_Community_Areas_20260122.csv"
+
+df = pd.read_csv(path)
+
+boundaries_gdf = gpd.GeoDataFrame(
+    df,
+    geometry=gpd.GeoSeries.from_wkt(df["the_geom"]),
+    crs="EPSG:4326"
+)
+
+fig, ax = plt.subplots(figsize=(8, 8))
+boundaries_gdf.plot(ax=ax, edgecolor="black", linewidth=0.6, facecolor="none")
+ax.set_axis_off()
+plt.show()
